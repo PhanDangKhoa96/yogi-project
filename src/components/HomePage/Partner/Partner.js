51dd1779f8f5ee37800b20b0f0ccf07a74d33b72
Khoa Phan <pldkhoa96.work@gmail.com>
2022-08-16 22:07:15 +0700

    feat: add partner section

--- src/components/HomePage/Partner/Partner.js
+++ src/components/HomePage/Partner/Partner.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const Partner = () => {
-    return <div>Partner</div>;
+    return (
+        <section className="container py-7">
+            <p className="description text-center w-2/3 mx-auto mb-5 md:mb-8 lg:mb-12">
+                Thousands of teams worldwide are using Hugi
+            </p>
+
+            <div className="flex flex-wrap gap-5 justify-center lg:gap-16 xl:gap-x-32">
+                <div className="w-[calc((100%-20px)/2)] lg:w-[calc((100%-3*64px)/4)] xl:w-[calc((100%-3*128px)/4)] text-center">
+                    <StaticImage
+                        objectFit="contain"
+                        src="../../../assets/google.svg"
+                        className="w-2/3 lg:w-full"
+                    />
+                </div>
+                <div className="w-[calc((100%-20px)/2)] lg:w-[calc((100%-3*64px)/4)] xl:w-[calc((100%-3*128px)/4)] text-center">
+                    <StaticImage
+                        objectFit="contain"
+                        src="../../../assets/yahoo.svg"
+                        className="w-2/3 lg:w-full"
+                    />
+                </div>
+
+                <div className="w-[calc((100%-20px)/2)] lg:w-[calc((100%-3*64px)/4)] xl:w-[calc((100%-3*128px)/4)] text-center">
+                    <StaticImage
+                        objectFit="contain"
+                        src="../../../assets/microsoft.svg"
+                        className="w-2/3 lg:w-full"
+                    />
+                </div>
+
+                <div className="w-[calc((100%-20px)/2)] lg:w-[calc((100%-3*64px)/4)] xl:w-[calc((100%-3*128px)/4)] text-center">
+                    <StaticImage
+                        objectFit="contain"
+                        src="../../../assets/huawei.svg"
+                        className="w-2/3 lg:w-full"
+                    />
+                </div> 
+            </div>
+        </section>
+    );
 };
 
 export default Partner;
